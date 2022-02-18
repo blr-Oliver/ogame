ba5cd8105c0cb18c7825825975ecaaa46fb6a7e1
blr-Oliver <vasily.liaskovsky@gmail.com>
2022-02-18 15:00:20 +0300

    added en l12n

--- src/common/translate.ts
+++ src/common/translate.ts
@@ -1,3 +1,4 @@
+import * as l18n_en from './l12n/en.json';
 import {InfoCategory, StringNumberMap} from './report-types';
 
 export type TranslationMapping = { [key: string]: string[] };
@@ -5,81 +6,7 @@ export type ReverseTranslationMapping = { [key: string]: string };
 export type CategorizedTranslationMapping = { [category in InfoCategory]: TranslationMapping };
 export type CategorizedReverseTranslationMapping = { [category in InfoCategory]: ReverseTranslationMapping };
 
-export const GLOBAL_TO_LOCAL: CategorizedTranslationMapping = {
-  resources: {
-    metal: ['металл'],
-    crystal: ['кристалл'],
-    deut: ['дейтерий'],
-    energy: ['энергия']
-  },
-  ships: {
-    lightFighter: ['лёгкий истребитель', 'легкий истребитель'],
-    heavyFighter: ['тяжёлый истребитель', 'тяжелый истребитель'],
-    cruiser: ['крейсер'],
-    battleship: ['линкор'],
-    battlecruiser: ['линейный крейсер'],
-    bomber: ['бомбардировщик'],
-    destroyer: ['уничтожитель'],
-    deathStar: ['звезда смерти'],
-    smallCargo: ['малый транспорт'],
-    largeCargo: ['большой транспорт'],
-    colonyShip: ['колонизатор'],
-    recycler: ['переработчик'],
-    espionageProbe: ['шпионский зонд'],
-    solarSatellite: ['солнечный спутник']
-  },
-  defense: {
-    rocketLauncher: ['ракетная установка'],
-    lightLaser: ['лёгкий лазер', 'легкий лазер'],
-    heavyLaser: ['тяжёлый лазер', 'тяжелый лазер'],
-    ionCannon: ['ионное орудие'],
-    gaussCannon: ['пушка гаусса'],
-    plasmaTurret: ['плазменное орудие'],
-    smallShield: ['малый щитовой купол'],
-    largeShield: ['большой щитовой купол'],
-    antiBallistic: ['ракета-перехватчик'],
-    interplanetary: ['межпланетная ракета']
-  },
-  buildings: {
-    metalMine: ['рудник по добыче металла'],
-    crystalMine: ['рудник по добыче кристалла'],
-    deutMine: ['синтезатор дейтерия'],
-    metalStorage: ['хранилище металла'],
-    crystalStorage: ['хранилище кристалла'],
-    deutStorage: ['ёмкость для дейтерия', 'емкость для дейтерия'],
-    solarPlant: ['солнечная электростанция'],
-    fusionReactor: ['термоядерная электростанция'],
-    robotics: ['фабрика роботов'],
-    nanite: ['фабрика нанитов'],
-    shipyard: ['верфь'],
-    researchLab: ['исследовательская лаборатория'],
-    terraformer: ['терраформер'],
-    allianceDepot: ['склад альянса'],
-    missileSilo: ['ракетная шахта'],
-    spaceDock: ['космический док'],
-    lunarBase: ['лунная база'],
-    sensorPhalanx: ['сенсорная фаланга'],
-    jumpGate: ['ворота']
-  },
-  research: {
-    energy: ['энергетическая технология'],
-    laser: ['лазерная технология'],
-    ion: ['ионная технология'],
-    hyperspace: ['гиперпространственная технология'],
-    plasma: ['плазменная технология'],
-    espionage: ['шпионаж'],
-    computer: ['компьютерная технология'],
-    astrophysics: ['астрофизика'],
-    intergalactic: ['межгалактическая исследовательская сеть'],
-    graviton: ['гравитационная технология'],
-    combustionDrive: ['реактивный двигатель'],
-    impulseDrive: ['импульсный двигатель'],
-    hyperspaceDrive: ['гиперпространственный двигатель'],
-    weaponsUpgrade: ['оружейная технология'],
-    shieldingUpgrade: ['щитовая технология'],
-    armorUpgrade: ['броня космических кораблей']
-  }
-};
+export const GLOBAL_TO_LOCAL: CategorizedTranslationMapping = l18n_en;
 export const LOCAL_TO_GLOBAL: CategorizedReverseTranslationMapping = reverseCategorizedMapping(GLOBAL_TO_LOCAL);
 
 export function translateEntries<T /*extends StringNumberMap*/>(category: InfoCategory, local?: StringNumberMap, padEntries: boolean = true, keepUnknown: boolean = true): T | undefined {
@@ -114,7 +41,9 @@ export function reverseTranslationMapping(categoryTranslation: TranslationMappin
 }
 export function reverseCategorizedMapping(mapping: CategorizedTranslationMapping): CategorizedReverseTranslationMapping {
   let result: { [category in InfoCategory]?: ReverseTranslationMapping } = {};
-  for (let category in mapping)
-    result[category as InfoCategory] = reverseTranslationMapping(mapping[category as InfoCategory]);
+  for (let category in mapping) {
+    if (category !== 'default')
+      result[category as InfoCategory] = reverseTranslationMapping(mapping[category as InfoCategory]);
+  }
   return result as CategorizedReverseTranslationMapping;
 }
