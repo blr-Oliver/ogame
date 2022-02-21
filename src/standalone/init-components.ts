7b411bd9613b715a108f06b74c1533f3d1fdf0ff
blr-Oliver <vasily.liaskovsky@gmail.com>
2022-02-21 17:27:53 +0300

    catch up standalone components initialization

--- src/standalone/init-components.ts
+++ src/standalone/init-components.ts
@@ -2,7 +2,8 @@ import {JSDOM} from 'jsdom';
 import {DOMEspionageReportParser} from '../browser/parsers/dom/espionage-report-dom';
 import {HtmlParser} from '../browser/parsers/dom/HtmlParser';
 import {JSONGalaxyParser} from '../browser/parsers/json/galaxy-report-json';
-import {cacheAsyncResult} from '../common/core/cached-async';
+import {CachingCostCalculator} from '../common/core/calculator/CostCalculator';
+import {FlightCalculator, StaticFlightCalculator} from '../common/core/calculator/FlightCalculator';
 import {GAME_PATH, LOBBY_DOMAIN_URL, LOBBY_LOGIN_URL, ServerContext} from '../common/core/ServerContext';
 import {UniverseContext} from '../common/core/UniverseContext';
 import {Analyzer} from '../common/services/Analyzer';
@@ -11,12 +12,20 @@ import {NoDOMPlayerContext} from '../common/services/context/NoDOMPlayerContext'
 import {NoDOMUniverseContext} from '../common/services/context/NoDOMUniverseContext';
 import {EspionageReportScrapper} from '../common/services/EspionageReportScrapper';
 import {GalaxyObserver} from '../common/services/GalaxyObserver';
+import {ReportProcessor} from '../common/services/ReportProcessor';
 import {Scanner} from '../common/services/Scanner';
 import {StatefulAutoObserve} from '../common/services/StatefulAutoObserve';
 import {LegacyMapper} from '../uncertain/LegacyMapper';
 import {LegacyFetcher} from './LegacyFetcher';
 import {SqlEspionageRepository} from './repository/SqlEspionageRepository';
 import {SqlGalaxyRepository} from './repository/SqlGalaxyRepository';
+
+export let universeContext: UniverseContext;
+export let autoRaid: AutoRaidImpl;
+export let analyzer: Analyzer;
+export let autoObserve: StatefulAutoObserve;
+export let flightCalculator: FlightCalculator;
+export let reportProcessor: ReportProcessor;
 
 export const GAME_DOMAIN = 's148-ru.ogame.gameforge.com';
 export const GAME_URL = 'https://' + GAME_DOMAIN + GAME_PATH;
@@ -31,23 +40,26 @@ export const serverContext: ServerContext = {
 }
 export const fetcher = new LegacyFetcher();
 export const playerContext = new NoDOMPlayerContext(serverContext, fetcher);
-export const universeContextSupplier = cacheAsyncResult(() => NoDOMUniverseContext.acquire(fetcher, serverContext));
-export let universeContext: UniverseContext;
-universeContextSupplier().then(result => universeContext = result);
+export const costCalculator = new CachingCostCalculator();
 export const espionageRepo = new SqlEspionageRepository();
 export const galaxyRepo = new SqlGalaxyRepository();
-export const galaxyRepoSupplier = () => Promise.resolve(galaxyRepo);
 export const htmlParser: HtmlParser = {
   parse(body: string): DocumentFragment {
     return JSDOM.fragment(body);
   }
-}
-export const espionageReportParser = new DOMEspionageReportParser(htmlParser);
+};
 export const galaxyParser = new JSONGalaxyParser();
+export const espionageReportParser = new DOMEspionageReportParser(htmlParser);
 export const espionageReportScrapper = new EspionageReportScrapper(espionageRepo, espionageReportParser, fetcher, serverContext);
-export const galaxyObserver = new GalaxyObserver(galaxyRepoSupplier, galaxyParser, fetcher, serverContext);
-export const autoObserve = new StatefulAutoObserve(galaxyObserver, galaxyRepoSupplier, universeContextSupplier);
 export const mapper = new LegacyMapper(fetcher, serverContext);
 export const scanner = new Scanner(playerContext, mapper);
-export const autoRaid = new AutoRaidImpl(playerContext, mapper, espionageReportScrapper, galaxyObserver, espionageRepo, galaxyRepo);
-export const analyzer = new Analyzer(playerContext, mapper, espionageRepo, galaxyRepo);
+export const galaxyObserver = new GalaxyObserver(galaxyRepo, galaxyParser, fetcher, serverContext);
+
+(async function () {
+  universeContext = await NoDOMUniverseContext.acquire(fetcher, serverContext);
+  flightCalculator = new StaticFlightCalculator(universeContext);
+  reportProcessor = new ReportProcessor(universeContext, flightCalculator, costCalculator);
+  autoRaid = new AutoRaidImpl(playerContext, mapper, mapper, espionageReportScrapper, galaxyObserver, espionageRepo, galaxyRepo, reportProcessor, flightCalculator);
+  analyzer = new Analyzer(playerContext, mapper, espionageRepo, galaxyRepo, reportProcessor);
+  autoObserve = new StatefulAutoObserve(galaxyObserver, galaxyRepo, universeContext);
+})();
