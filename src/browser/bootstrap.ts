20fcae5f36e450f32a9a925b4fad962d7b897e51
blr-Oliver <vasily.liaskovsky@gmail.com>
2022-02-24 14:15:24 +0300

    lighter page bootstrap

--- src/browser/bootstrap.ts
+++ src/browser/bootstrap.ts
@@ -1,6 +1,4 @@
 import {getCurrentClientId} from '../common/client-id';
-import {CachingCostCalculator} from '../common/core/calculator/CostCalculator';
-import {StaticFlightCalculator} from '../common/core/calculator/FlightCalculator';
 import {NativeFetcher} from '../common/core/NativeFetcher';
 import {RestrainedFetcher} from '../common/core/RestrainedFetcher';
 import {IDBRepositoryProvider} from '../common/idb/IDBRepositoryProvider';
@@ -10,19 +8,9 @@ import {IDBGalaxyRepository} from '../common/idb/repositories/IDBGalaxyRepositor
 import {IDBGalaxyRepositorySupport} from '../common/idb/repositories/IDBGalaxyRepositorySupport';
 import {MessageChannelWithFactory} from '../common/message/MessageChannelWithFactory';
 import {AutoObserveStub} from '../common/remote/AutoObserveStub';
-import {AjaxEventListLoader} from '../common/services/AjaxEventListLoader';
 import {LocationServerContext} from '../common/services/context/LocationServerContext';
-import {NoDOMPlayerContext} from '../common/services/context/NoDOMPlayerContext';
-import {NoDOMUniverseContext} from '../common/services/context/NoDOMUniverseContext';
 import {EspionageReportScrapper} from '../common/services/EspionageReportScrapper';
-import {GalaxyObserver} from '../common/services/GalaxyObserver';
-import {Raider} from '../common/services/Raider';
-import {RaidReportAnalyzer} from '../common/services/RaidReportAnalyzer';
-import {ReportProcessor} from '../common/services/ReportProcessor';
-import {TwoStepLauncher} from '../common/services/TwoStepLauncher';
-import {JSONGalaxyParser} from './parsers/json/galaxy-report-json';
 import {NoDOMEspionageReportParser} from './parsers/no-dom/espionage-report-no-dom';
-import {NoDOMEventListParser} from './parsers/no-dom/event-list-no-dom';
 import {ServiceWorkerConnector} from './ServiceWorkerConnector';
 
 if ('serviceWorker' in navigator) {
@@ -47,8 +35,6 @@ if ('serviceWorker' in navigator) {
   (window as any)['autoObserve'] = autoObserve;
   const fetcher = new RestrainedFetcher(new NativeFetcher());
   const serverContext = new LocationServerContext(window.location);
-  const playerContext = new NoDOMPlayerContext(serverContext, fetcher);
-  const launcher = new TwoStepLauncher(serverContext, fetcher);
 
   const galaxyRepositorySupport = new IDBGalaxyRepositorySupport();
   const espionageRepositorySupport = new IDBEspionageRepositorySupport();
@@ -56,32 +42,17 @@ if ('serviceWorker' in navigator) {
     'galaxy': galaxyRepositorySupport,
     'espionage': espionageRepositorySupport
   });
-  const galaxyParser = new JSONGalaxyParser();
   const espionageParser = new NoDOMEspionageReportParser();
-  const eventListParser = new NoDOMEventListParser();
-  const eventListLoader = new AjaxEventListLoader(fetcher, eventListParser, serverContext);
 
   Promise.all([
     repositoryProvider.getRepository<IDBEspionageRepository>('espionage'),
     repositoryProvider.getRepository<IDBGalaxyRepository>('galaxy'),
-    NoDOMUniverseContext.acquire(fetcher, serverContext)
-  ]).then(([espionageRepo, galaxyRepo, universe]) => {
-    const galaxyObserver = new GalaxyObserver(galaxyRepo, galaxyParser, fetcher, serverContext);
-    const flightCalculator = new StaticFlightCalculator(universe);
-    const costCalculator = new CachingCostCalculator();
-    const reportProcessor = new ReportProcessor(universe, flightCalculator, costCalculator);
+  ]).then(([espionageRepo, galaxyRepo]) => {
     (window as any)['espionageRepo'] = espionageRepo;
     (window as any)['galaxyRepo'] = galaxyRepo;
     const espionageScrapper = new EspionageReportScrapper(espionageRepo, espionageParser, fetcher, serverContext);
     (window as any)['espionageScrapper'] = espionageScrapper;
-    const analyzer = new RaidReportAnalyzer(universe, flightCalculator, costCalculator);
-    const raider = new Raider(playerContext, galaxyRepo, espionageRepo, espionageScrapper, eventListLoader, analyzer, launcher);
-    (window as any)['raider'] = raider;
-    raider.minFreeSlots = 2;
-    raider.maxRaidSlots = 15;
   });
-  (window as any)['launcher'] = launcher;
-  (window as any)['playerContext'] = playerContext;
 } else {
   console.error('Service workers not supported.')
 }
